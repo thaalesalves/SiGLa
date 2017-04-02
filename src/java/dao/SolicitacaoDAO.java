3bcc5ceacd833538b1ceb6f399abaf2994c68878
user <user@USER-PC.umc.br>
2017-04-02 14:00:05 -0300

    SELECT many-to-many frito na camada DAO de Solicitacao para seleção de todas as reservas

--- src/java/dao/SolicitacaoDAO.java
+++ src/java/dao/SolicitacaoDAO.java
@@ -27,7 +27,15 @@ import model.Solicitacao;
 import util.DatabaseConnection;
 
 public class SolicitacaoDAO {
-    
+
+    private final String INSERT_AUX = "INSERT INTO sw_soli VALUES(NEXTVAL('seq_sw_soli'), ?, ?)";
+    private final String INSERT_RETURN = "INSERT INTO solicitacao VALUES(NEXTVAL('seq_soli'), ?, ?, ?, ?, ?, ?, ?) RETURNING id";
+    private final String SELECT_ID = "SELECT s.id AS soli, s.qtd_alunos, s.turma, s.professor, s.modulo, s.dia_semana, s.obs, curso.id AS c_id, curso.modalidade AS modalidade, curso.nome AS curso, software.id AS sw_id, software.fabricante AS fabricante, software.nome AS software FROM solicitacao AS s INNER JOIN curso ON (curso.id = s.curso) INNER JOIN sw_soli ON (sw_soli.res = s.id) INNER JOIN software ON (software.id = sw_soli.sw) WHERE s.id = ?";
+    private final String SELECT_ALL = "SELECT s.id AS soli, s.qtd_alunos, s.turma, s.professor, s.modulo, s.dia_semana, s.obs, curso.id AS c_id, curso.modalidade AS modalidade, curso.nome AS curso, software.id AS sw_id, software.fabricante AS fabricante, software.nome AS software FROM solicitacao AS s INNER JOIN curso ON (curso.id = s.curso) INNER JOIN sw_soli ON (sw_soli.res = s.id) INNER JOIN software ON (software.id = sw_soli.sw)";
+    private final String DELETE = "DELETE FROM solicitacao WHERE id = ?";
+    private final String DELETE_AUX = "DELETE FROM sw_soli WHERE res = ?";
+    private final String COUNT = "SELECT COUNT(id) FROM solicitacao";
+
     public Solicitacao selectSolicitacao(Solicitacao s) throws SQLException, ClassNotFoundException {
         try (Connection conn = DatabaseConnection.getConnection()) {
             PreparedStatement pstmt = conn.prepareStatement("SELECT curso.id AS curso_id, software.id AS sw_id, s.qtd_alunos AS qtd_alunos, s.turma AS turma, s.professor AS professor, s.modulo AS modulo, s.dia_semana AS dia_semana, s.obs AS obs, software.fabricante AS fabricante, software.nome AS software, curso.modalidade AS modalidade, curso.nome AS curso FROM software, curso, solicitacao AS s WHERE s.softwares = software.id AND s.curso = curso.id AND s.id = ?");
@@ -58,25 +66,11 @@ public class SolicitacaoDAO {
         
         return s;
     }
-    
-    public void deleteSolicitacao(Solicitacao s) throws SQLException, ClassNotFoundException {
-        try (Connection connString = DatabaseConnection.getConnection()) {
-            PreparedStatement pstmt = connString.prepareStatement("DELETE FROM solicitacao WHERE id = ?");
-            
-            pstmt.setInt(1, s.getId());
-            
-            pstmt.executeUpdate();
-            
-            connString.close();
-        } catch (Exception e) {
-            util.Logger.logSevere(e, this.getClass());
-        }
-    }
-    
+
     public Solicitacao insertSolicitacoes(Solicitacao s) throws SQLException, ClassNotFoundException {
         try (Connection connString = DatabaseConnection.getConnection()) {
-            PreparedStatement pstmt = connString.prepareStatement("INSERT INTO solicitacao VALUES(NEXTVAL('seq_soli'), ?, ?, ?, ?, ?, ?, ?, ?) RETURNING id");
-            
+            PreparedStatement pstmt = connString.prepareStatement(INSERT_RETURN);
+
             pstmt.setInt(1, s.getSoftware().getId()); // software
             pstmt.setInt(2, s.getCurso().getId()); // curso
             pstmt.setInt(3, s.getQtdAlunos()); // qtd de alunos
@@ -85,26 +79,26 @@ public class SolicitacaoDAO {
             pstmt.setString(6, s.getModulo()); // modulo
             pstmt.setString(7, s.getDiaSemana()); // dia da semana
             pstmt.setString(8, s.getObservacao()); // observacao
-            
+
             ResultSet rs = pstmt.executeQuery();
-            
+
             while (rs.next()) {
                 s.setId(rs.getInt(1));
             }
-            
+
             connString.close();
         } catch (Exception e) {
             util.Logger.logSevere(e, this.getClass());
         }
-        
+
         return s;
     }
-    
+
     public int countSolicitacoes() throws SQLException, ClassNotFoundException {
         int qtd = 0;
 
         try (Connection connString = DatabaseConnection.getConnection()) {
-            PreparedStatement pstmt = connString.prepareStatement("SELECT COUNT(*) FROM solicitacao");
+            PreparedStatement pstmt = connString.prepareStatement(COUNT);
             ResultSet rs = pstmt.executeQuery();
 
             while (rs.next()) {
@@ -123,27 +117,25 @@ public class SolicitacaoDAO {
         ArrayList<Solicitacao> arrayRes = new ArrayList<Solicitacao>();
 
         try (Connection connString = DatabaseConnection.getConnection()) {
-            PreparedStatement pstmt = connString.prepareStatement("SELECT curso.id AS curso_id, software.id AS sw_id, s.qtd_alunos AS qtd_alunos, s.id AS solicitacao, s.turma AS turma, s.professor AS professor, s.modulo AS modulo, s.dia_semana AS dia_semana, s.obs AS obs, software.fabricante AS fabricante, software.nome AS software, curso.modalidade AS modalidade, curso.nome AS curso FROM software, curso, solicitacao AS s WHERE s.softwares = software.id AND s.curso = curso.id ORDER BY s.id DESC");
+            PreparedStatement pstmt = connString.prepareStatement(SELECT_ALL);
             ResultSet rs = pstmt.executeQuery();
 
             while (rs.next()) {
-                Solicitacao r = new Solicitacao();
+                Solicitacao s = new Solicitacao();
 
-                r.setId(rs.getInt("solicitacao"));
-                r.setQtdAlunos(rs.getInt("qtd_alunos"));
-                r.getCurso().setId(rs.getInt("curso_id"));
-                r.getCurso().setModalidade(rs.getString("modalidade"));
-                r.getCurso().setNome(rs.getString("curso"));
-                r.getSoftware().setId(rs.getInt("sw_id"));
-                r.getSoftware().setFabricante(rs.getString("fabricante"));
-                r.getSoftware().setNome(rs.getString("software"));
-                r.getPessoa().setUsername(rs.getString("professor"));
-                r.setDiaSemana(rs.getString("dia_semana"));
-                r.setObservacao(rs.getString("obs"));
-                r.setTurma(rs.getString("turma"));
-                r.setModulo(rs.getString("modulo"));
+                s.setId(rs.getInt("soli"));
+                s.setQtdAlunos(rs.getInt("qtd_alunos"));
+                s.setTurma(rs.getString("turma"));
+                s.getPessoa().setUsername(rs.getString("professor"));
+                s.setModulo(rs.getString("modulo"));
+                s.setDiaSemana(rs.getString("dia_semana"));
+                s.setObservacao(rs.getString("obs"));
+                s.getCurso().setModalidade(rs.getString("modalidade"));
+                s.getCurso().setNome(rs.getString("curso"));
+                s.getSoftware().setFabricante(rs.getString("fabricante"));
+                s.getSoftware().setNome(rs.getString("software"));
 
-                arrayRes.add(r);
+                arrayRes.add(s);
             }
 
             connString.close();
@@ -152,5 +144,25 @@ public class SolicitacaoDAO {
         }
 
         return arrayRes;
+    }
+
+    public void deleteSolicitacao(Solicitacao s) throws SQLException, ClassNotFoundException {
+        try (Connection connString = DatabaseConnection.getConnection()) {
+            PreparedStatement pstmt = connString.prepareStatement(DELETE_AUX);
+
+            pstmt.setInt(1, s.getId());
+
+            pstmt.executeUpdate();
+
+            pstmt = connString.prepareStatement(DELETE);
+
+            pstmt.setInt(1, s.getId());
+
+            pstmt.executeUpdate();
+
+            connString.close();
+        } catch (Exception e) {
+            util.Logger.logSevere(e, this.getClass());
+        }
     }
 }
