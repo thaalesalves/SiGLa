d5a0c8dea893b8478533d002ad0895fb35fa89eb
Thales Alves <thaalesalves@outlook.com>
2017-07-25 08:23:14 +0000

    Takipi removido

--- run_nossl.cmd
+++ run_nossl.cmd
@@ -10,4 +10,4 @@ IF [%1]==[] (
 
 title Instancia do SiGLa (porta %port%) - Sem SSL
 
-mvn package && cls && java -Dcom.sun.jndi.ldap.connect.pool.timeout=1800000 -agentlib:TakipiAgent -cp target/dependency/jetty-runner.jar org.eclipse.jetty.runner.Runner --port %port% target/*.war
+mvn package && cls && java -Dcom.sun.jndi.ldap.connect.pool.timeout=1800000 -cp target/dependency/jetty-runner.jar org.eclipse.jetty.runner.Runner --port %port% target/*.war
