418252686a4df1f9b9a19209ec053ac84471baa2
thaal <thaal@NOTEBOOK-PC.home>
2017-08-28 00:19:38 -0300

    Movida validação de grupos para o topo

--- src/main/java/controller/actions/LoginAction.java
+++ src/main/java/controller/actions/LoginAction.java
@@ -44,63 +44,50 @@ public class LoginAction implements ICommand {
     public String execute(HttpServletRequest request, HttpServletResponse response) throws SQLException, ConnectException, IOException, NamingException, ServletException {
         HttpSession session = request.getSession();
         try {
-            ActiveDirectory ad = new ActiveDirectory();
-            Pessoa p = new Pessoa();
-
             System.setProperty("javax.net.ssl.keyStorePassword", "changeit");
             System.setProperty("com.sun.jndi.ldap.connect.pool.timeout", "1800000");
             System.setProperty("javax.net.ssl.trustStore", SiGLa.KEYSTORE);
-            System.setProperty("javax.net.ssl.keyStore", SiGLa.KEYSTORE);       
+            System.setProperty("javax.net.ssl.keyStore", SiGLa.KEYSTORE);
 
-            p.setUsername(request.getParameter("username").replaceAll("[0-9]", "")); // passa o atributo de usuário
-            p.setSenha(request.getParameter("password")); // passa o atributo de senha           
+            ActiveDirectory ad = new ActiveDirectory();
+            Pessoa p = new Pessoa();
+            p.setUsername(request.getParameter("username").replaceAll("[0-9]", "")); 
+            p.setSenha(request.getParameter("password"));          
 
-            if (ad.login(p)) { // faz o login
+            if (ad.login(p)) { 
                 GrupoDAO gdao = new GrupoDAO();
                 ArrayList<Grupo> arrayg = gdao.select();
-                ArrayList<Pessoa> ps = ad.getData();
+                boolean acesso = false;
+
+                for (Grupo g : arrayg) {
+                    g.setGrupo(arrayg.get(arrayg.indexOf(g)).getGrupo());
+                    if (ad.isMember(p, arrayg.get(arrayg.indexOf(g))) && acesso == false) {
+                        p.setRole(g.getRole());
+                        acesso = true;
+                        break;
+                    }
+                }
+
+                if (!acesso) {
+                    session.setAttribute("msg", "Voc&ecirc; n&atilde;o tem permiss&atilde;o de acesso");
+                    session.setAttribute("status", "error");
+                    return request.getContextPath();
+                }
+
+                ArrayList<Pessoa> ps = ad.getProfessores();
                 session.setAttribute("todos-usuarios", ps);
 
                 p.setNome(ad.getGivenName(p)); // passa o atributo de nome
                 p.setNomeCompleto(ad.getCN(p)); // passa o atributo de nome completo                
                 p.setCargo(ad.getTitle(p)); // passa o atributo de cargo
                 p.setDepto(ad.getDepartment(p)); // passa o atributo de cargo 
-                p.setEmail(p.getUsername() + "@sigla.thalesalv.es"); // passa o atributo de email 
+                p.setEmail(p.getUsername() + "@" + SiGLa.getDomain()); // passa o atributo de email 
                 p.setShownName(p.getNome() + " " + p.getNomeCompleto().substring(p.getNomeCompleto().lastIndexOf(" ") + 1));
 
-                boolean args = false;
-                while (args) {
-                    try {
-                        p.setPicture(ad.getPicture(p));
-                        FileOutputStream path = new FileOutputStream(new File(SiGLa.WEBAPP + "/img/user/" + p.getUsername() + "_pic.jpg"));
-                        path.write(p.getPicture());
-                        path.close();
-                    } catch (Exception e) {
-                        continue;
-                    }
-                }
-                
-                boolean acesso = false;
-                for (Grupo g : arrayg) {
-                    g.setGrupo(arrayg.get(arrayg.indexOf(g)).getGrupo());
-                    if (ad.isMember(p, arrayg.get(arrayg.indexOf(g)))) {
-                        p.setRole(g.getRole());
-                        acesso = true;
-                    }
-                }
-
-                if (acesso) {
-                    session.setAttribute("ad", ad); // salva dados do AD na sessão
-                    session.setAttribute("pessoa", p); // salva dados do login na sessão
-                    return request.getContextPath() + "/pagina/home";
-                } else {
-                    session.setAttribute("msg", "Voc&ecirc; n&atilde;o tem permiss&atilde;o de acesso");
-                    session.setAttribute("status", "error");
-                    return request.getContextPath(); // chama de volta a página de login
-                }
+                session.setAttribute("ad", ad); // salva dados do AD na sessão
+                session.setAttribute("pessoa", p); // salva dados do login na sessão
+                return request.getContextPath() + "/pagina/home";
             }
-
-            System.out.println("Login para " + p.getNomeCompleto() + " autorizado.");
         } catch (CommunicationException e) {
             util.Logger.logSevere(e, this.getClass());
             session.setAttribute("msg", "Erro ao contactar a controladora de dom&iacute;nio");
