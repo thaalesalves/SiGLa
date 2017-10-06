d15c04c0115f4c8f79b3e7b0657abbecb659304a
thaal <thaal@NOTEBOOK-PC.home>
2017-10-06 19:49:43 -0300

    Corrigidos métodos de entrada e saída

--- src/main/java/util/IO.java
+++ src/main/java/util/IO.java
@@ -1,7 +1,21 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+/**
+ * Copyright (C) 2016 Thales Alves Pereira
+ *
+ *  This file is part of SiGla.
+ *
+ *   SiGla is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   SiGla is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with SiGLa.  If not, see <http://www.gnu.org/licenses/>.
+ *
  */
 package util;
 
@@ -13,28 +27,24 @@ public class IO {
     static Scanner in = new Scanner(System.in);
     static PrintStream out = new PrintStream(System.out);
 
-    public static String read(String value) {
+    public static String read() {
         return in.next();
     }
 
-    public static String readln(String value) {
+    public static String readln() {
         return in.nextLine();
     }
 
-    public static String read(Integer value) {
-        return in.next();
+    public static Integer readInt() {
+        return in.nextInt();
     }
 
-    public static String readln(Integer value) {
-        return in.nextLine();
+    public static Float readFloat() {
+        return in.nextFloat();
     }
-
-    public static String read(Double value) {
-        return in.next();
-    }
-
-    public static String readln(Double value) {
-        return in.nextLine();
+    
+    public static Double readDouble() {
+        return in.nextDouble();
     }
 
     public static void write(String value) {
@@ -53,6 +63,14 @@ public class IO {
         out.println(value);
     }
 
+    public static void write(Float value) {
+        out.print(value);
+    }
+    
+    public static void writeln(Float value) {
+        out.println(value);
+    }
+    
     public static void write(Double value) {
         out.print(value);
     }
